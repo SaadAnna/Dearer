572ce28d24248ec1e2d0d0827d7acb80c8c5238b
saad <saadannadev@gmail.com>
2024-07-17 00:30:20 +0200

    modified Header.js and Style.css file and add four image file

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import MYimage from "./pexels-kseniachernaya-3965545.jpg"
+import IMAGE from "./1.png"
+import myImage from "./2.png"
+import Image from "./3.png"
+import IMageProduct from "./pexels-frendsmans-1926769.jpg"
 import "./Style.css";
  export default function Header(){
    return (
      <div>
 <div id="Header">
- <div>
+ <div>  
  <h1>
     welcome!
  </h1>
@@ -42,8 +46,8 @@ Contact Us For More.
     </div>
       <div id="Our_services">
       <div className="Frist_Services">
-      <svg xmlns="http://www.w3.org/2000/svg" className="IMAge" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
-      <h1>saad anna</h1>
+      <img className="Image" src={IMAGE} alt="My Image" />
+      <h1>Good Cmmunication.</h1>
       <h5>
       Lorem ipsum dolor sit amet, consectetur <br/>
        adipiscing elit. Donec at cursus nunc <br/>
@@ -52,29 +56,60 @@ Contact Us For More.
          lacus. Sed fermentum.
       </h5>
       </div>
-      <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
-      <h1>saad anna</h1>
-      <h1>
+      <div className="Frist_Services">
+      <img className="Image" src={myImage} alt="My Image" />
+      <h1>Advertisement.
+
+      </h1>
+      <h5>
       Lorem ipsum dolor sit amet, consectetur <br/>
        adipiscing elit. Donec at cursus nunc <br/>
        varius dapibus elit Nullam sem velit, efficitur <br/>
         id ullamcorper eu, dignissim a <br/>
          lacus. Sed fermentum.
-      </h1>
+      </h5>
       </div>
-      <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
-      <h1>saad anna</h1>
-      <h1>
+      <div className="Frist_Services">
+      <img className="Image" src={Image} alt="My Image" />
+      <h1>Good Surveillance.</h1>
+      <h5>
       Lorem ipsum dolor sit amet, consectetur <br/>
        adipiscing elit. Donec at cursus nunc <br/>
        varius dapibus elit Nullam sem velit, efficitur <br/>
         id ullamcorper eu, dignissim a <br/>
          lacus. Sed fermentum.
-      </h1>
+      </h5>
       </div>
       </div>
+      <div id="Product">
+     <div>
+     <h1>
+     our products
+     </h1>
+     <h5>
+     Some of our famous <br/>
+      and best-selling 
+       products locally and <br/>
+        internationally.
+     </h5>
+     </div>
+    </div> 
+    <div id="Our_Product">
+      <div class="card">
+      <div>
+  <h1>White Chemise.</h1>
+  <p className="Description">A White Shirt For <br/>
+   Women From Adidas <br/>
+   Elegant And Distinctive<br/>  For Important 
+    Occasions <br/>As Well As For Outings 
+    <br/> And Family Gatherings.</p>
+  <p className="price">$129.99</p>
+
+  <p><button>Add To Cart</button></p>
+  </div>
+   <img src={IMageProduct}></img>
+</div>
+    </div>
      </div>
    
    )
