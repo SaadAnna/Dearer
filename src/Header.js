0ed8b55c494d435cd25ae36673bfd89d20f2c047
saad <saadannadev@gmail.com>
2024-07-16 12:35:59 +0200

    add image file and javascript file and midified react.js file

---
+++ src/Header.js
@@ -0,0 +1,84 @@
+import MYimage from "./pexels-kseniachernaya-3965545.jpg"
+import "./Style.css";
+ export default function Header(){
+   return (
+     <div>
+<div id="Header">
+ <div>
+ <h1>
+    welcome!
+ </h1>
+ <h5>
+ Buy Everything You Need <br/>  In The World Of Clothing <br/>  From One Place.
+ </h5>
+ <a href="saad">
+   Shop
+ </a>
+ </div>
+
+ 
+</div>
+<div id="Image">
+  
+<img src={MYimage} alt="My Image" />
+</div>
+     <div id="Services">
+     <div>
+     <h1>
+     WHAT WE DO
+     </h1>
+     <h5>
+     We Are A Brand Of Casual <br/>  Clothing And Footwear 
+      With A Wonderful <br/>And Distinctive Design   
+       We Sell Clothes <br/>And We Also Print Your   
+        Design On  <br/> Any Type Of 
+         Clothing You Choose <br/>
+Contact Us For More.
+     </h5>
+     <a href="saad">
+       shop
+     </a>
+     </div>
+    </div>
+      <div id="Our_services">
+      <div className="Frist_Services">
+      <svg xmlns="http://www.w3.org/2000/svg" className="IMAge" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
+      <h1>saad anna</h1>
+      <h5>
+      Lorem ipsum dolor sit amet, consectetur <br/>
+       adipiscing elit. Donec at cursus nunc <br/>
+       varius dapibus elit Nullam sem velit, efficitur <br/>
+        id ullamcorper eu, dignissim a <br/>
+         lacus. Sed fermentum.
+      </h5>
+      </div>
+      <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
+      <h1>saad anna</h1>
+      <h1>
+      Lorem ipsum dolor sit amet, consectetur <br/>
+       adipiscing elit. Donec at cursus nunc <br/>
+       varius dapibus elit Nullam sem velit, efficitur <br/>
+        id ullamcorper eu, dignissim a <br/>
+         lacus. Sed fermentum.
+      </h1>
+      </div>
+      <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill:"white"}} viewBox="0 0 24 24"><path d="M18 6c0 2.972-2.164 5.433-5 5.91v8.09h-2v-8.089c-2.836-.477-5-2.938-5-5.911 0-3.314 2.687-6 6-6s6 2.687 6 6zm-2 0c0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4 4-1.794 4-4zm-5.618 2.098c2.339 1.84 5.563-.722 3.858-3.539.313 2.237-1.956 4.03-3.858 3.539zm9.618 7.902h-5v2h3.764l.5 1h-4.264v1.175l.783 1.825h-7.488l.705-1.643v-1.357h-2.042l-1.011-1h3.053v-2h-5l-4 8h24l-4-8zm-12.794 6h-3.97l1.764-3.528 1.516 1.528h1.549l-.859 2zm8.808-2h3.75l1 2h-3.892l-.858-2z"/></svg>
+      <h1>saad anna</h1>
+      <h1>
+      Lorem ipsum dolor sit amet, consectetur <br/>
+       adipiscing elit. Donec at cursus nunc <br/>
+       varius dapibus elit Nullam sem velit, efficitur <br/>
+        id ullamcorper eu, dignissim a <br/>
+         lacus. Sed fermentum.
+      </h1>
+      </div>
+      </div>
+     </div>
+   
+   )
+
+
+
+}
---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import "./Style.css";
+export default function Nav(){
+return (
+    <div>
+      <header>
+        <a href="saad" id="logo">dearer</a>
+           <input type="checkbox" id="nav_check" hidden />
+           <nav>
+               <ul>
+                   <li>
+                       <a href="saad" className="a-color" >Home</a>
+                   </li>
+                   <li>
+                       <a href="saad">Poduct</a>
+                   </li>
+                   <li>
+                       <a href="saad" class="">Contact Us</a>
+                   </li>
+                   <li>
+                       <a href="about-page.html">About Us</a>
+                   </li>
+                   <li>
+                       <a href="about-page.html">Blog</a>
+                   </li>
+
+               </ul>
+           </nav>
+           <label for="nav_check" class="hamburger">
+               <div class="hamburger-ligne"></div>
+               <div class="hamburger-ligne"></div>
+               <div class="hamburger-ligne"></div>
+           </label>
+       </header>
+    </div>
+)
+
+}
